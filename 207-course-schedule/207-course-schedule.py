c3ef0e2d9960c06dbdc02b03ba0a795bdbbbf0a8
teyouale <teyouale@gmail.com>
2022-04-10 10:08:59 +0300

    Time: 156 ms (38.78%), Space: 17.3 MB (13.62%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -2,17 +2,21 @@ from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        indegree = [0]*numCourses
         
         for a,b in prerequisites:
             graph[b].append(a)
-            indegree[a]+=1
-        queue = [i for i in range(len(indegree)) if indegree[i]==0]
-        # count=0
-        # while  queue:
-        for j in queue:
-            for i in graph[j]:
-                indegree[i]-=1
-                if indegree[i] == 0:
-                    queue.append(i)
-        return len(queue)==numCourses
+            
+        visted , cycle = set(),set()
+        def dfs(course):
+            if course in cycle:return False
+            if course in visted:return True
+            
+            cycle.add(course)
+            for i in graph[course]:
+                if not dfs(i):return False
+            cycle.remove(course)
+            visted.add(course)
+            return True
+        for i in range(numCourses):
+            if not dfs(i):return False
+        return True
