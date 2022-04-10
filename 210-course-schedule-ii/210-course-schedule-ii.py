dfa0000d3e2048ea7286f233faa782fd7fc2a53a
teyouale <teyouale@gmail.com>
2022-04-10 10:04:25 +0300

    Time: 141 ms (52.83%), Space: 17.6 MB (9.11%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -2,17 +2,21 @@ from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        indegree = [0]*numCourses
-        
         for a,b in prerequisites:
             graph[b].append(a)
-            indegree[a]+=1
-        queue = [i for i in range(numCourses) if indegree[i]== 0 ]
-        for i in queue:
-            for j in graph[i]:
-                indegree[j]-=1
-                if indegree[j]==0:
-                    queue.append(j)
-        if len(queue) == numCourses:
-            return queue 
-        else:return []
+        res = []
+        visted , cycle = set(),set()
+        def dfs(course):
+            if course in cycle: return False
+            if course in visted:return True
+            cycle.add(course)
+            for pre in graph[course]:
+                if not dfs(pre):return False
+            cycle.remove(course)
+            visted.add(course)
+            res.append(course)
+            return True
+        for i in range(numCourses):
+            if not dfs(i):return []
+        return res[::-1]
+        
