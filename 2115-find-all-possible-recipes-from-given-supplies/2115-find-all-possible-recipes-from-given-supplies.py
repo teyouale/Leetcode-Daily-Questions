a5cd2391417841d2540c32481ab3cf7fffe0d882
teyouale <teyouale@gmail.com>
2022-04-11 20:01:47 +0300

    Time: 1487 ms (30.34%), Space: 17 MB (70.80%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -3,19 +3,19 @@ class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
         # return []
         graph = defaultdict(list)
-        indegree = [0]*len(recipes)
+        indegree = defaultdict(int)
         res = []
         queue = supplies[:]
-        for k,(r,i) in enumerate((zip(recipes,ingredients))):
+        for r,i in zip(recipes,ingredients):
             for j in i:
                 graph[j].append(r)
-                indegree[k]+=1
+                indegree[r]+=1
         while queue:
             current = queue.pop()
             if current in recipes:
                 res.append(current)
             for neg in graph[current]:
-                indegree[recipes.index(neg)]-=1
-                if indegree[recipes.index(neg)] == 0:
+                indegree[neg]-=1
+                if indegree[neg] == 0:
                     queue.append(neg)
         return res
