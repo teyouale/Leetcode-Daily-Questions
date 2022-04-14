782737d6268b53828152f5de08a5702ae81599c7
teyouale <teyouale@gmail.com>
2022-04-14 18:58:56 +0300

    Time: 201 ms (87.41%), Space: 14.1 MB (97.04%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected[0])
+        root =[i for i in range(n)]
+        
+#         Quick Union approch
+
+        def find(x):
+            while x != root[x]:
+                x = root[x]
+            return x
+        
+        def union(x,y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX != rootY:
+                root[rootY] = rootX
+                
+        for g,i in enumerate(isConnected):
+            for k,j in enumerate(i):
+                if g != k and j == 1:
+                    union(g,k)
+        return len(set([find(i) for i in range(n)]))
