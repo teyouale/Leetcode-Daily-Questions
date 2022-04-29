244ffac8b7491532b671592bca5cdd558f7b0938
teyouale <teyouale@gmail.com>
2022-04-29 19:09:44 +0300

    Time: 100 ms (19.88%), Space: 14.2 MB (5.71%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -1,5 +1,31 @@
-from collections import Counter
+from collections import defaultdict
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnding = False
+        self.count = 0
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self,word):
+        node = self.root
+        for char in word:
+            if char in  node.children:
+                node = node.children[char]
+            else:
+                newNode = TrieNode()
+                node.children[char] = newNode
+                node = newNode
+        node.isEnding = False
+        node.count+=1 
+        return node.count
 class Solution:
-    def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        c = Counter(words)
-        return[x for x,y in  sorted(c.most_common(), key = lambda x: (-x[1], x[0]))[:k]]
+    def topKFrequent(self, words: List[str], limit: int) -> List[str]:
+        res = defaultdict(int)
+        obj = Trie()
+        for k,v in enumerate(words):
+            c = obj.insert(v)
+            res[v] = c
+     
+        res = Counter(res)
+        return[x for x,y in  sorted(res.most_common(), key = lambda x: (-x[1], x[0]))][:limit]
