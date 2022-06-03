aa1f05456489ed5e029d7cbf309d19a29cbb8d2a
teyouale <teyouale@gmail.com>
2022-06-03 20:58:57 +0300

    Time: 1427 ms (98.83%), Space: 25.1 MB (40.31%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,19 +1,22 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.prefixSum = []
-        
-        for m in matrix:
-            temp = [0]
-            for x in m:
-                temp.append(temp[-1]+x)
-            self.prefixSum.append(temp)
-
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        ROW = len(matrix)
+        COL = len(matrix[0])
+        self.prefixSum = [[0] * (COL+1) for r in range(ROW+1)]  
+        for r,m in enumerate(matrix):
+            temp = 0
+            for c,i in enumerate(m):
+                above = self.prefixSum[r][c+1]
+                temp += i
+                self.prefixSum[r+1][c+1] = above+temp
+    def sumRegion(self, r1: int, c1: int, r2: int, c2: int) -> int:
         res = 0
-        for i in range(row1,row2+1):
-            res+=self.prefixSum[i][col2+1]- self.prefixSum[i][col1]
-        return res
+        end = self.prefixSum[r2+1][c2+1]
+        above = self.prefixSum[r1][c2+1]
+        side = self.prefixSum[r2+1][c1]
+        intersection = self.prefixSum[r1][c1]
+        return end+intersection - side - above
 
 
 # Your NumMatrix object will be instantiated and called as such:
