de845d3892b7d9c111e484c0f2329d3d2d0ec8c0
teyouale <teyouale@gmail.com>
2022-07-08 23:08:28 +0300

    Time: 40 ms (68.20%), Space: 13.9 MB (56.71%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        res = ''
+        curr = columnNumber
+        while curr > 0:
+            x , n = divmod(curr-1,26)
+            curr = x
+            # res.append(chr(65+n))
+            res = ''.join((chr(65+n), res))
+        return ''.join(res)
